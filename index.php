cba41fafc5b266d5e9b0a2bd14d4b7b6b6c83b1c
Ahmad Alrifai <45950857+ahmad-alrifai@users.noreply.github.com>
2023-07-11 03:48:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ahmad Alrifai</title>
+    </head>
+    <body>
+        <h1>
+            Hi!
+        </h1><br>
+        <h3>
+            This website is still under construction.
+        </h3>
+    </body>
+</html>
